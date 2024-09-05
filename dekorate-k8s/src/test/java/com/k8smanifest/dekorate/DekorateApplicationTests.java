b166d7264106294be6b9fdb719f755f8eaf39fd0
pra9989 <pramod.katla@trianz.com>
2024-09-05 16:28:58 +0530

    manifest k8s

---
+++ dekorate-k8s/src/test/java/com/k8smanifest/dekorate/DekorateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8smanifest.dekorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekorateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
