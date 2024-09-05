118d68d130421a448af216785017c080ff60c48f
Pramod Katla (Trianz) <pramod.katla@trianz.com>
2024-09-05 14:31:12 +0530

    adding the dekorate maven project

---
+++ dekorate-manifest/src/main/java/com/manifestexample/dekorate_manifest/DekorateManifestApplication.java
@@ -0,0 +1,13 @@
+package com.manifestexample.dekorate_manifest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DekorateManifestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DekorateManifestApplication.class, args);
+	}
+
+}
