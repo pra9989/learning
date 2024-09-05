32bce77c5d104b15595fe861d8e6c6fa26622e9b
pra9989 <pramod.katla@trianz.com>
2024-09-05 16:21:18 +0530

    spring boot java application

---
+++ dekorate/dekorate/src/test/java/com/exampletest/dekorate/DekorateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampletest.dekorate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DekorateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
