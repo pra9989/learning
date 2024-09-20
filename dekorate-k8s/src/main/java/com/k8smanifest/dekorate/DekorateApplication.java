3acc0cc66baa4cf114cda8dd8af020443a013f16
pra9989 <36298255+pra9989@users.noreply.github.com>
2024-09-20 18:07:23 +0530

    Update DekorateApplication.java
    added dekorate details

--- dekorate-k8s/src/main/java/com/k8smanifest/dekorate/DekorateApplication.java
+++ dekorate-k8s/src/main/java/com/k8smanifest/dekorate/DekorateApplication.java
@@ -1,9 +1,17 @@
 package com.k8smanifest.dekorate;
 
+import io.dekorate.kubernetes.annotation.KubernetesApplication;
+import io.dekorate.kubernetes.annotation.Label;
+import io.dekorate.kubernetes.annotation.Port;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@KubernetesApplication(
+	ports = @Port(name = "http", containerPort = 9090),
+       labels = @Label(key = "version", value = "v1"),
+        expose = false
+)
 public class DekorateApplication {
 
 	public static void main(String[] args) {
